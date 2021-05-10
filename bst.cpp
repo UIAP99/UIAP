c799cdea0c3f05c9b39bb3248b315846bd1659b6
FarahaniMehrshad <mehrshad.farahani.jafari@gmail.com>
2021-05-10 13:43:50 +0430

    Some changes to increase code readability

--- bst.cpp
+++ bst.cpp
@@ -1,22 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class node
+class Node
 {
 	private:
 		int data;
-		node *parent;
-		node *right;
-		node *left;
+                Node* parent;
+                Node* right;
+                Node* left;
 	public:
-		node(int data, node *parent = nullptr, node *right = nullptr, node *left = nullptr)
+                Node(int data, Node* parent = nullptr, Node* right = nullptr, Node* left = nullptr)
         {
             this->data = data;
             this->parent = parent;
             this->right = right;
             this->left = left;
         }
-        int get_data()const
+        int get_data()
         {
             return data;
         }
@@ -24,27 +24,27 @@ class node
         {
             this->data = data;
         }
-        node *get_parent()const
+        Node* get_parent()
         {
             return parent;
         }
-        void set_parent(node *parent)
+        void set_parent(Node *parent)
         {
             this->parent = parent;
         }
-        node *get_right()const
+        Node* get_right()
         {
             return right;
         }
-        void set_right(node *right)
+        void set_right(Node *right)
         {
             this->right = right;
         }
-        node *get_left()const
+        Node* get_left()
         {
             return left;
         }
-        void set_left(node *left)
+        void set_left(Node *left)
         {
             this->left = left;
         }
@@ -53,18 +53,18 @@ class node
 class Binary_Search_Tree
 {
 	private:
-		node *root;
+                Node* root;
 		int size;
 	public:
-		int get_size()const
-        {
-            return size;
-        }
+            int get_size()
+            {
+                return size;
+            }
 		Binary_Search_Tree();
 		~Binary_Search_Tree();
 		void insert(int data);
 		void remove(int data);
-		node *find(int data);
+                Node *find(int data);
 		//search about bfs
 		void print();
 };
