d83ba38a88aea5d1aca3255495193de4d304b34f
FarahaniMehrshad <mehrshad.farahani.jafari@gmail.com>
2021-05-17 02:10:45 +0430

    Add insert function

--- non-recursive-bst.cpp
+++ non-recursive-bst.cpp
@@ -28,7 +28,53 @@ public:
     {
         return size;
     }
+    bool insert(int data)
+    {
+        Node* new_node{new Node(data)};
 
+        if(root == nullptr)
+        {
+            root = new_node;
+            size++;
+            return true;
+        }
+
+        Node* tempnode{root};
+        while(true)
+        {
+            if(data < tempnode->data)
+            {
+
+                if(tempnode->left == nullptr)
+                {
+                    tempnode->left = new_node;
+                    new_node->parent = tempnode;
+                    size++;
+                    return true;
+                }
+
+                tempnode = tempnode->left;
+            }
+
+            if(data > tempnode->data)
+            {
+                if(tempnode->right == nullptr)
+                {
+                    tempnode->right = new_node;
+                    new_node->parent = tempnode;
+                    size++;
+                    return true;
+                }
+
+                tempnode = tempnode->right;
+            }
+
+            if(data == tempnode->data)
+            {
+                return false;
+            }
+        }
+    }
 
 };
 
