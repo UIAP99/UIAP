2ec4a2dc6a21a08abe8561613f6236af2d4225d4
FarahaniMehrshad <mehrshad.farahani.jafari@gmail.com>
2021-05-17 01:58:36 +0430

    Add Node class and BST class

---
+++ non-recursive-bst.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Node{
+    
+private:
+    int data;
+    Node* parent;
+    Node* right;
+    Node* left;
+    
+    
+};
+
+class BST{
+    
+private:
+    Node* root;
+    int size;
+};
+
+
+int main()
+{
+    
+    
+    
+    
+    return 0;
+}
