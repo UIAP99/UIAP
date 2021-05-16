1fba3e27cc4b2835f951fea6fe4532c868833d44
FarahaniMehrshad <mehrshad.farahani.jafari@gmail.com>
2021-05-17 02:07:45 +0430

    Add constructor function to node class and some other small changes.

--- non-recursive-bst.cpp
+++ non-recursive-bst.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 
 class Node{
-    
+
+    friend class BST;
 private:
     int data;
     Node* parent;
     Node* right;
     Node* left;
-    
-    
+    Node(int data)
+    {
+        this->data = data;
+        parent = nullptr;
+        right = nullptr;
+        left = nullptr;
+    }
 };
 
 class BST{
-    
+
 private:
     Node* root;
     int size;
+
+public:
+    int get_size()
+    {
+        return size;
+    }
+
+
 };
 
 
 int main()
 {
-    
-    
-    
-    
+
+
+
     return 0;
 }
